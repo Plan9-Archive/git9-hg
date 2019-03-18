c8a32672da1f64315a47d8f952fc41d7d9a2cb11
Ori Bernstein <ori@eigenstate.org>
2019-03-17 22:56:06 -0700

    Fix directory modes in commits.

--- pack.c
+++ pack.c
@@ -529,6 +529,7 @@ parsetree(Object *o)
 		o->ent = realloc(o->ent, ++o->nent * sizeof(Dirent));
 		t = &o->ent[o->nent - 1];
 		m = strtol(buf, nil, 8);
+		/* FIXME: symlinks and other BS */
 		t->mode = m & 0777 | ((m & 0040000) ? DMDIR : 0);
 		t->name = p;
 		nn = strlen(p) + 1;
